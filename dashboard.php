34ada6e3ceca932a4d72b2b361ef7f3b6b4c0eb6
afiiqkacak <66316306+afiiqkacak@users.noreply.github.com>
2020-06-09 08:43:50 +0800

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -19,11 +19,11 @@ GROUP BY category_of_car
 ORDER BY frequency DESC";
 $result2 = mysqli_query($connect,$sql2) or die (mysqli_error($connect));
 
-$sql3 = "SELECT service_type, COUNT(service_type) AS total
+$sql3 = 'SELECT service_type, COUNT(service_type) AS total
 FROM queue_service INNER JOIN service INNER JOIN queue
-WHERE queue_service.queue_id=queue.queue_id AND service.service_id=queue_service.service_id AND date=CURDATE()
+WHERE queue_service.queue_id=queue.queue_id AND service.service_id=queue_service.service_id AND date=DATE_FORMAT(SYSDATE(),"%Y-%c-%d")
 GROUP BY service_type
-ORDER BY total DESC";
+ORDER BY total DESC';
 $result3 = mysqli_query($connect,$sql3) or die (mysqli_error($connect));
 
 $sql4 = "SELECT category_of_car AS kereta, DATE_FORMAT(SEC_TO_TIME(AVG(TIME_TO_SEC(TIMEDIFF(completed_time,arrival_time)))),'%H:%i') AS avgtime
@@ -34,7 +34,7 @@ $result4 = mysqli_query($connect,$sql4) or die (mysqli_error($connect));
 
 $sql5 = "SELECT DATE_FORMAT(arrival_time, '%H') AS time, COUNT(queue_id) AS jumlah
 FROM `queue`
-WHERE date=CURDATE()
+WHERE date=DATE_FORMAT(SYSDATE(),'%Y-%c-%d')
 GROUP BY time";
 $result5 = mysqli_query($connect,$sql5) or die (mysqli_error($connect));
 ?>
