c7d72fee89c210d99ff58ddbab25c39c8df1b981
afiiqkacak <66316306+afiiqkacak@users.noreply.github.com>
2020-06-14 19:14:39 +0800

    Update dbcheck.php

--- dbcheck.php
+++ dbcheck.php
@@ -3,19 +3,22 @@ include('connection/connect.php');
 $sql3 = "SELECT * FROM queue ORDER BY queue_id DESC LIMIT 1";
 $result2 = mysqli_query($connect, $sql3);
 if (mysqli_num_rows($result2)) {
-	$masa=$result2['completed_time'];
+	while($row2 = mysqli_fetch_array($result2)) {
+	$masa=$row2['completed_time'];
 	echo $masa;
 
 	$time=time();
-	echo "$time";
+	echo $time;
+	}
 
 	// $timestampCheck = time() - 120;
 	// if ($result3['completed_time'] >= $timestampCheck){
 
 	// 	echo "<audio src='bell.mp3' controls autoplay></audio>";
 
-}
-else{
+
+}else{
+	echo 'tak jadi';
 	
 }
 ?>
