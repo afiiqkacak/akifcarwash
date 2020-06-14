f90db1fe2fac075b7cc8db2d405c5e6d429f9a63
afiiqkacak <66316306+afiiqkacak@users.noreply.github.com>
2020-06-14 19:59:01 +0800

    Update dbcheck.php

--- dbcheck.php
+++ dbcheck.php
@@ -1,12 +1,12 @@
 <?php
 date_default_timezone_set('Asia/Kuala_Lumpur');
 include('connection/connect.php');
-$sql3 = "SELECT * FROM queue ORDER BY queue_id DESC LIMIT 1";
+$sql3 = "SELECT *, TIME_FORMAT(completed_time, '%H:%i') AS masa FROM queue ORDER BY queue_id DESC LIMIT 1";
 $result2 = mysqli_query($connect, $sql3);
 if (mysqli_num_rows($result2)) {
 	while($row2 = mysqli_fetch_array($result2)) {
 
-	$masa=$row2['completed_time'];
+	$masa=$row2['masa'];
 	echo $masa;
 	// $memasa=$masa("H:i");
 	// echo $memasa;
