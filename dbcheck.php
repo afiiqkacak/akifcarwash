668e3b79398694d9ef7e268e50d59487b77514e1
afiiqkacak <66316306+afiiqkacak@users.noreply.github.com>
2020-06-14 19:32:58 +0800

    Update dbcheck.php

--- dbcheck.php
+++ dbcheck.php
@@ -6,19 +6,20 @@ $result2 = mysqli_query($connect, $sql3);
 if (mysqli_num_rows($result2)) {
 	while($row2 = mysqli_fetch_array($result2)) {
 
-	// $masa=$row2['completed_time'];
-	// echo $masa;
-	// echo '\n';
+	$masa=$row2['completed_time'];
+	$memasa=$masa("H:i");
+	echo $memasa;
+	echo '\n';
 
-	$time=date("H:i:s");
-	// echo $time;
+	$time=date("H:i");
+	echo $time;
 
-	$timestampCheck = $time - 120;
-		if ($masa >= $timestampCheck){
-				echo "<audio src='bell.mp3' controls autoplay></audio>";
-	}else{
-		echo "listening";
-	}
+	// $timestampCheck = $time - 120;
+	// 	if ($masa >= $timestampCheck){
+	// 			echo "<audio src='bell.mp3' controls autoplay></audio>";
+	// }else{
+	// 	echo "listening";
+	// }
 }
 
 	
