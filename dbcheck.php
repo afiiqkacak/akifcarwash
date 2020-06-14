2df73610a67e08b311c6a01f7b22ffe7d5c55dee
afiiqkacak <66316306+afiiqkacak@users.noreply.github.com>
2020-06-14 19:46:21 +0800

    Update dbcheck.php

--- dbcheck.php
+++ dbcheck.php
@@ -6,9 +6,9 @@ $result2 = mysqli_query($connect, $sql3);
 if (mysqli_num_rows($result2)) {
 	while($row2 = mysqli_fetch_array($result2)) {
 
-	// $masa=$row2['completed_time'];
+	$masa=$row2['completed_time'];
 	$memasa = strtotime('$masa');
-	echo date('G:i', $memasa);
+	echo date('H:i', $memasa);
 	// $memasa=$masa("H:i");
 	// echo $memasa;
 	echo '\n';
