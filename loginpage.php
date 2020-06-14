42f2b9cf647ab29d025befa2e8b2ceb85dec002f
afiiqkacak <66316306+afiiqkacak@users.noreply.github.com>
2020-06-15 06:18:40 +0800

    Update loginpage.php

--- loginpage.php
+++ loginpage.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>akifcarwash</title>
+	<title>AKIF CARWASH</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
